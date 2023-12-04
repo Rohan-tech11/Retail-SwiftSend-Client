dafa758be20510a5d9329640eadf0cc93cd43d77
Mayank Thite <mayank.thite98@gmail.com>
2023-12-04 16:44:29 -0500

    add dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,19 @@
-export default function Dashboard() {
-    return(
-        <div>
-            This is Dashboard.
-        </div>
-    )
+import { jwtDecode } from "jwt-decode";
+import { useState } from "react";
+
+export default function Dashboard({ token }) {
+  const [user, setUser] = useState("");
+
+  if (!token) {
+    throw new Response(
+      JSON.stringify({
+        message: "You do not have permission to access this page.",
+      }),
+      { status: 403 }
+    );
+  } else {
+    setUser(jwtDecode(token));
+  }
+
+  return <div>{user ? user.email : ""}</div>;
 }
