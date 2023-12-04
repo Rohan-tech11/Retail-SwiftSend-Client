eaecf76f66094de788ad6509178270c0d6be55d4
Mayank Thite <mayank.thite98@gmail.com>
2023-12-04 16:42:47 -0500

    add login button

--- src/pages/Verify.jsx
+++ src/pages/Verify.jsx
@@ -1,15 +1,17 @@
-import styles from './Verify.module.css';
+import { Link } from "react-router-dom";
+import styles from "./Verify.module.css";
 
 export default function Verify() {
   return (
     <div className={styles.container}>
-      <h1>
-        Please check your email for a verification link to login.
-      </h1>
+      <h1>Please check your email for a verification link to login.</h1>
       <p>
         If you have verified and are still not able to access your account,
         please contact support at admin@swiftsend.com
       </p>
+      <Link to="/login">
+        <button className={styles.button}>Login Here</button>
+      </Link>
     </div>
   );
 }
