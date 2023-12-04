fe8eaf8079215059d9dd16f0030f0b9efd53c4d9
Mayank Thite <mayank.thite98@gmail.com>
2023-12-04 16:46:54 -0500

    import new pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,43 @@ import Homepage from "./pages/Homepage";
 import About from "./pages/About";
 import Verify from "./pages/Verify";
 import ClientSignup from "./pages/ClientSignup";
+import AddService from "./pages/AddService";
+import Dashboard from "./pages/Dashboard";
+import ErrorPage from "./pages/ErrorPage";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Services from "./pages/Services";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <BaseLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
         element: <Homepage />,
       },
       { path: "login", element: <Login /> },
-      { path: "signup", element: <SignUp /> },
+      {
+        path: "signup",
+        element: <SignUp />,
+      },
+      { path: "signup/client", element: <ClientSignup /> },
       { path: "about", element: <About /> },
       { path: "verify", element: <Verify /> },
-      { path: "signup/client", element: <ClientSignup /> },
+      { path: "dashboard", element: <Dashboard /> },
+    ],
+  },
+  {
+    path: "client",
+    element: <DashboardLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "services",
+        element: <Services />,
+        children: [{ path: "add", element: <AddService /> }],
+      },
     ],
   },
 ]);
