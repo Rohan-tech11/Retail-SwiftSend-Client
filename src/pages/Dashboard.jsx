c278d4141088e64e5fb0fc3dc37322aa8a4d8046
Mayank Thite <mayank.thite98@gmail.com>
2023-12-05 20:45:07 -0500

    add user routes and pages

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,3 @@
-import { jwtDecode } from "jwt-decode";
-import { useState } from "react";
-
-export default function Dashboard({ token }) {
-  const [user, setUser] = useState("");
-
-  if (!token) {
-    throw new Response(
-      JSON.stringify({
-        message: "You do not have permission to access this page.",
-      }),
-      { status: 403 }
-    );
-  } else {
-    setUser(jwtDecode(token));
-  }
-
-  return <div>{user ? user.email : ""}</div>;
+export default function Dashboard() {
+    return <h1>User dashboard.</h1>
 }
