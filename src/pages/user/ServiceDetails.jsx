5b273dc910c4419028d3032e37c9887d26e33400
Mayank Thite <mayank.thite98@gmail.com>
2023-12-13 01:24:14 -0500

    api integration

--- src/pages/user/ServiceDetails.jsx
+++ src/pages/user/ServiceDetails.jsx
@@ -1,8 +1,10 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
 import styles from "./ServiceDetails.module.css";
 import { useEffect, useState } from "react";
 import OrderForm from "./OrderForm";
+import getAuthToken from "../../utils/auth";
 
 const services = [
   {
@@ -79,35 +81,55 @@ const services = [
 
 export default function ServiceDetails() {
   const { serviceId } = useParams();
-  //   console.log(serviceId);
 
   const [service, setService] = useState("");
+  const [banner, setBanner] = useState(false);
 
-  // Service name
-  // Service description
-  // client name
-  // price per km
-  function fetchService() {
+  async function fetchService() {
     setService(services[serviceId - 1]);
+    const token = getAuthToken().token;
+
+    await axios({
+      method: "get",
+      url: `/api/users/fetchServiceById/${serviceId}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        console.log(res.data.responseData);
+        setService(res.data.responseData);
+      })
+      .catch((err) => {
+        console.log("Error fetching service", err);
+      });
   }
 
   useEffect(() => {
     fetchService();
   }, []);
-  console.log(service);
 
   // place order form
   return (
     <div>
       <div className={styles.header}>
-        <h1>{service.name}</h1>
-        <p>{service.clientName}</p>
+        <h1>{service.serviceName}</h1>
+        <p>{service.service_provider}</p>
       </div>
       <div className={styles.belowHeader}>
-        <p>{service.serviceType}</p>
+        <p>{service.serviceType?.toUpperCase()}</p>
         <p>${service.price}/km</p>
       </div>
-      <OrderForm />
+      <p className={styles.description}>{service.serviceDescription}</p>
+      {banner && (
+        <div className={styles.successBanner}>
+          Order Placed Successfully!{" "}
+          <Link to="/user/orders" className={styles.link}>
+            View
+          </Link>{" "}
+        </div>
+      )}
+      <OrderForm id={serviceId} banner={banner} setBanner={setBanner} />
     </div>
   );
 }
