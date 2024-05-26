cc050f24c5ede63a123666a6aa25b1c2e600eaf4
Harikrishnan-HK <krishnanhari@outlook.com>
2024-05-26 00:36:24 -0400

    img updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import logo from '/logo-glyph.png';
 export default function Footer() {
   return (
     <FooterContainer>
-      Copyright &copy; 2023. All rights reserved | Team SwiftSend{' '}
+      Copyright &copy; 2024. All rights reserved | Team SwiftSend{' '}
       <img src={logo} alt="logo" width="15px" />
     </FooterContainer>
   );
