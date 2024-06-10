8c674d2db1289de2ec807c92efa05d6d60bd7925
Harikrishnan-HK <krishnanhari@outlook.com>
2024-06-10 14:36:43 -0400

    footer made hidded - login, signup

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,14 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function BaseLayout() {
+  const location = useLocation();
+
+  const hideFooterPaths = ["/signup", "/login", "/signup/client"];
+
+  const isFooterHidden = hideFooterPaths.includes(location.pathname);
+
   return (
     <>
       <Header />
       <Outlet />
-      <Footer />
+      {!isFooterHidden && <Footer />}
     </>
   );
 }
